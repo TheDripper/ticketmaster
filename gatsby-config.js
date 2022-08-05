9243460aaa17e28f1f4ed5e89df4f551215872cb
TheDripper <deadcustomer@Deads-MacBook-Air.local>
2022-08-05 14:41:08 -0700

    load tailwind, filter for baseball

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     title: `ticketmaster`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-postcss"]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,26 +5,46 @@ const IndexPage = () => {
   const [events, setEvents] = useState(null);
   useEffect(() => {
     (async () => {
-      const data = await fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=Is8qRbhQMZ2EQ0e6a86GfBPg79vMDqb1");
+      const data = await fetch(
+        "https://app.ticketmaster.com/discovery/v2/events.json?keyword=baseball&apikey=Is8qRbhQMZ2EQ0e6a86GfBPg79vMDqb1"
+      );
       const response = await data.json();
-      console.log('response',response);
-      setEvents(response._embedded.events);
-  })();
-}, []);
+      console.log("response", response);
+      let returnedEvents = [];
+      for (let event of response._embedded.events) {
+        let eventData = {
+          id: event.id,
+          name: event.name,
+          startDate: event.dates.start.localDate,
+          startTime: event.dates.start.localTime,
+          thumbnail: event.images[0].url,
+          link: event.url,
+        };
+        returnedEvents.push(eventData);
+      }
+      setEvents(returnedEvents);
+    })();
+  }, []);
 
-if (events === null) {
-  return null;
-}
-    
+  if (events === null) {
+    return null;
+  }
+
   return (
     <main>
-      {events.length > 0 ? (
-        events.map(event => (
-          <p key={event.id}>{event.name}</p>
-        ))
-      ) : (
-        <p>No events</p>
-      )}
+      <div id="events" class="w-full p-8">
+        {events.length > 0 ? (
+          events.map((event) => (
+            <div class="event border w-1/2 relative">
+              <img src={event.thumbnail} />
+              <p key={event.id}>{event.name}</p>
+              <a href={event.link} class="cursor-pointer absolute w-full h-full top-0 left-0 z-10"></a>
+            </div>
+          ))
+        ) : (
+          <p>No events</p>
+        )}
+      </div>
     </main>
   );
 };
